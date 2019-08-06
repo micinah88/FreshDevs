f0262a60bd0892f3950bce354fa50e0905687350
Micole Ong <micoleinah@gmail.com>
2019-08-05 20:47:56 -0700

    Added user model and controller for api

---
+++ webApi/Migrations/201908060342098_Initial.cs
@@ -0,0 +1,27 @@
+﻿namespace webApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ webApi/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace webApi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<webApi.Models.webApiContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(webApi.Models.webApiContext context)
+        {
+            
+        }
+    }
+}
