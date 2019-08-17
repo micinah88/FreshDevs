519ca7b109ef30d1134f9dbb094e8950bb36872e
graham-huelscher <graham.huelscher@gmail.com>
2019-08-16 18:47:11 -0700

    final version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { Menu, Button, Form, Container } from 'semantic-ui-react'
+import { Menu, Button, Form, Container, Header, Table } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 import UserController from './controllers/UserController'
 
 class App extends Component {
 
-  state = { activeItem: 'Add User' }
+  state = {
+    activeItem: 'Add User',
+    users: []
+  }
+
+  componentDidMount() {
+    this.getUserList()
+  }
+
+  getUserList = async () => {
+    const users = await UserController.getUsers()
+    this.setState({
+      users
+    })
+  }
 
   onSubmit = async (e) => {
     e.preventDefault();
@@ -15,21 +29,37 @@ class App extends Component {
       FirstName: e.target.FirstName.value,
       LastName: e.target.LastName.value
     }
-    let response;
-    if(this.state.activeItem === 'Update User') {
+    e.target.FirstName.value = ""
+    e.target.LastName.value = ""
+    if (this.state.activeItem === 'Update User') {
       userObj.ID = e.target.ID.value
-      response = await UserController.updateUser(userObj)
+      await UserController.updateUser(userObj)
+      e.target.ID.value = ""
     }
     else
-      response = await UserController.addUser(userObj)
+      await UserController.addUser(userObj)
 
-    console.log(response)
+    this.getUserList();
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
     const { activeItem } = this.state
+
+    const userTableJSX = this.state.users.map(user => {
+      return (<Table.Row>
+        <Table.Cell>
+          <Header as='h2' textAlign='center'>
+            {user.ID}
+          </Header>
+        </Table.Cell>
+        <Table.Cell singleLine>{user.FirstName}</Table.Cell>
+        <Table.Cell singleLine>{user.LastName}</Table.Cell>
+      </Table.Row>)
+
+    })
+
     return (
       <Container>
         <Menu tabular>
@@ -37,7 +67,7 @@ class App extends Component {
           <Menu.Item name='Update User' active={activeItem === 'Update User'} onClick={this.handleItemClick} />
         </Menu>
 
-        <Form onSubmit={this.onSubmit}>
+        <Form autocomplete="off" onSubmit={this.onSubmit}>
           <Form.Field>
             <label>First Name</label>
             <input name="FirstName" placeholder='First Name' />
@@ -47,16 +77,30 @@ class App extends Component {
             <input name="LastName" placeholder='Last Name' />
           </Form.Field>
           {this.state.activeItem === 'Update User' ? <Form.Field>
-                                                        <label>User Id</label>
-                                                        <input type='number' name="ID" placeholder='42' />
-                                                        </Form.Field> 
-                                                    : null }
+            <label>User Id</label>
+            <input type='number' name="ID" placeholder='42' />
+          </Form.Field>
+            : null}
           <Button type='submit'>Submit</Button>
         </Form>
+
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell singleLine>ID</Table.HeaderCell>
+              <Table.HeaderCell>First Name</Table.HeaderCell>
+              <Table.HeaderCell>Last Name</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {userTableJSX}
+          </Table.Body>
+        </Table>
       </Container>
     )
   }
-
 }
 
 export default App;
+
--- frontend/src/controllers/UserController.js
+++ frontend/src/controllers/UserController.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
 const UserController = {
+  getUsers: async () => {
+    const response = await axios.get('/api/users')
+    return response.data
+  },
   addUser: async (newUser) => {
       const response = await axios.post(`/api/users`, newUser)
       return response.data
